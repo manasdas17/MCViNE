9da7a7d30c518ced93e580357eeaf962db3a2e0e
Jiao Lin <linjiao@caltech.edu>
2011-03-25 15:32:22 +0000

    ARCS/simulations/resolution-calculator: added directory "mod2sample"
    git-svn-id: svn://danse.us/MCViNE/trunk@1070 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ instruments/ARCS/simulations/resolution-calculator/mod2sample/run.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+
+def execute(cmd):
+    import os
+    print '* executing %s... ' % cmd
+    if os.system(cmd):
+        raise RuntimeError, "%r failed" % cmd
+
+
+def run(Ei, ncount):
+    # generate configration
+    cmd = """
+    arcs-m2s \
+        --fermi_nu=600 \
+        --fermi_bladeradius=0.5801 \
+        --T0_nu=120 \
+        --E=%(Ei)s \
+        --emission_time=0 \
+        --- \
+        -dump-pml
+    """ % locals()
+    execute(cmd)
+
+
+    # fine tune configuraiton
+    cmd = "arcs_moderator2sample --overwrite-datafiles --dump-pml"
+    execute(cmd)
+
+
+    # run simulation
+    import os, shutil
+    if os.path.exists('out'):
+        shutil.rmtree('out')
+    cmd = "arcs_moderator2sample -ncount=%(ncount)s " % locals()
+    # ... moderator data file
+    moddat = os.path.join(
+        os.environ['MCVINE_DIR'], 'share', 'mcvine',
+        'instruments', 'ARCS', 'source_sct521_bu_17_1.dat',
+        )
+    cmd += '-moderator.S_filename=%s' % moddat
+    cmd += '> m2s.log 2> m2s.err'
+    execute(cmd)
+
+
+    # analyze output
+    # ... number of neutrons left
+    neutronfile = os.path.join('out', 'neutrons')
+    from mcni.neutron_storage.idf_usenumpy import count
+    ncountatsample = count(neutronfile)
+    
+    cmd = [
+        'arcs_analyze_beam',
+        '-source.path=%(neutronfile)s' % locals(),
+        '-ncount=%(ncountatsample)s' % locals(),
+        '--output-dir=out-analyzer',
+        ]
+    # ... compute enegy range
+    from mcni.utils.conversion import e2v
+    v = e2v(Ei)
+    from pyre.units.time import second
+    L = 13.6
+    t = L/v
+    # ... build command line with monitor parameters
+    cmd += ['--monitor.mtof.tofmin=%s' % (t*0.9)]
+    cmd += ['--monitor.mtof.tofmax=%s' % (t*1.1)]
+    cmd += ['--monitor.mtof.ntof=%s' % (1000)]
+    cmd += ['--monitor.menergy.energymin=%s' % (Ei*0.9)]
+    cmd += ['--monitor.menergy.energymax=%s' % (Ei*1.1)]
+    cmd += ['--monitor.menergy.nenergy=%s' % (1000)]
+    # ... run
+    cmd = ' '.join(cmd)
+    execute(cmd)
+    return
+
+
+def main():
+    import sys
+    Ei = float(sys.argv[1])
+    ncount = float(sys.argv[2])
+    run(Ei, ncount)
+    return
+
+
+if __name__ == '__main__': main()
+
