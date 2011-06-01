55039ad1416ff3186c269ab1565e91890fbe988f
Jiao Lin <linjiao@caltech.edu>
2011-06-01 12:51:11 +0000

    arcs/simulations/monosource-idealrecoil-storage: now has a good test script to try a bunch of cases of hits and misses
    git-svn-id: svn://danse.us/MCViNE/trunk@1175 bd03dbb6-ca95-436e-b6ad-03a95796df24

--- instruments/ARCS/simulations/He4/monosource-idealrecoil-storage/run.py
+++ instruments/ARCS/simulations/He4/monosource-idealrecoil-storage/run.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 
+r = 0.01; h = 0.02
+
+
+
 def execute(cmd):
     import os
     print '* executing %s... ' % cmd
@@ -17,11 +21,83 @@ def run():
         )
 
     #
-    import os, shutil
-    if os.path.exists('out'):
-        shutil.rmtree('out')
-    runsim(0,0)
+    from numpy import arange
+    for x in arange(-r*0.95, r*0.95, r/3.):
+        for y in arange(-h/2*0.95, h/2*0.95, h/3):
+            checkHit(x,y)
+            continue
+        continue
+
+    epsilon = 1e-3
+    x = r * (1+epsilon)
+    for y in arange(-h/2*0.95, h/2*0.95, h/5):
+        checkMiss(x,y)
+        continue
+    x = r * (-1-epsilon)
+    for y in arange(-h/2*0.95, h/2*0.95, h/5):
+        checkMiss(x,y)
+        continue
+    y = h/2 * (1+epsilon)
+    for x in arange(-r*0.95, r*0.95, r/5):
+        checkMiss(x,y)
+        continue
+    y = -h/2 * (1+epsilon)
+    for x in arange(-r*0.95, r*0.95, r/5):
+        checkMiss(x,y)
+        continue
+
     return
+
+
+def checkMiss(x,y):
+    print '*'*3, "checkMiss:", x,y; 
+    epsilon = 1e-4
+    def eq(x,y): return abs(x-y)<epsilon
+    N = 10
+    for i in range(N):
+        runsim(x,y)
+        neutron = readNeutron()
+        
+        position = tuple(neutron.state.position)
+        x1,y1,z = position
+        print position
+        assert eq(x1,x) and eq(y1,y) and eq(z,-13.6), str(position)
+        
+        continue
+    return
+
+
+def checkHit(x,y):
+    print '*'*3, "checkHit:", x,y; 
+    #
+    N = 10
+    epsilon = 1e-4
+    for i in range(N):
+        runsim(x,y)
+        neutron = readNeutron()
+        
+        velocity = tuple(neutron.state.velocity)
+        vx, vy, vz = velocity
+        
+        position = tuple(neutron.state.position)
+        x1,y1,z1 = position
+
+        assert x1!=0 or y1!=0 or z1!=r
+
+        assert abs(x1*x1+z1*z1) <= r*r*(1+epsilon), "position=%s, x^2=%s, z^2=%s, x^2+z^2=%s, r^2=%s" % (
+            position, x1*x1, z*z, x1*x1+z1*z1, r*r)
+        assert abs(y1) <= h/2 * (1+epsilon)
+        continue
+    
+    return
+
+
+def readNeutron():
+    neutrons = 'out/scattered-neutrons'
+    from mcni.neutron_storage import load
+    neutrons = load(neutrons)
+    assert len(neutrons) == 1
+    return neutrons[0]
 
     
 def runsim(
@@ -29,6 +105,9 @@ def runsim(
     ):
     """source neutrons come from (x,y) going to direction z
     """
+    import os, shutil
+    if os.path.exists('out'):
+        shutil.rmtree('out')
     # run main sim
     cmd = './sss --source.position="%s,%s,0"' % (x,y,)
     execute(cmd)
