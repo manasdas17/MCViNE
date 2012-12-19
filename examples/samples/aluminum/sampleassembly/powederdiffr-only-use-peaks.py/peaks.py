1cb6d2f2f47b3a8bf19a0b06279d7d3260312f4b
Jiao Lin <linjiao@caltech.edu>
2012-12-19 01:19:29 +0000

    examples/samples/aluminum: added example using SimplePowderDiffractionKernel.laz-path
    git-svn-id: svn://danse.us/MCViNE/trunk@1401 bd03dbb6-ca95-436e-b6ad-03a95796df24

---
+++ examples/samples/aluminum/sampleassembly/powederdiffr-only-use-peaks.py/peaks.py
@@ -0,0 +1,1 @@
+../../powderdiffr/peaks.py
--- examples/samples/aluminum/scatterer-xml/powderdiffr-only.xml.use-peaks.py
+++ examples/samples/aluminum/scatterer-xml/powderdiffr-only.xml.use-peaks.py
@@ -5,7 +5,7 @@
 <!-- weights: absorption, scattering, transmission -->
 <homogeneous_scatterer mcweights="0, 1, 0">
 
-  <SimplePowderDiffractionKernel Dd_over_d="1e-5" DebyeWaller_factor="0" laz-path="Al.laz">
+  <SimplePowderDiffractionKernel Dd_over_d="1e-5" DebyeWaller_factor="0" peaks-py-path="peaks.py">
   </SimplePowderDiffractionKernel>
   
 </homogeneous_scatterer>
